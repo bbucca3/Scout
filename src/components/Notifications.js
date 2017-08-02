b8afc84da0a2ed40d8c16006569a5b4665cdad0e
Nick Akey <nick@nickakey.com>
2017-08-02 16:04:00 -0700

    Skeleton of notifications class created

---
+++ src/components/Notifications.js
@@ -0,0 +1,40 @@
+import geolib from 'geolib';
+
+
+class Notifications {
+  constructor() {
+    this.notifications = [];
+    this.notificationsToBeFired = [];
+    this.distanceValuesToMeters = {
+      1: 30,
+      2: 152,
+      3: 609,
+      4: 3218,
+      5: 8046,
+      6: 16093,
+      7: 40233
+    }
+  }
+
+  fetchNotifications(userid, geolocation){
+    //do a get request to the server
+  }
+
+  intervalFetchNotifications(milliseconds){
+  }
+
+  activateNotifications(){
+    //scroll through this.notifications, 
+    //and if one is within distance
+  }
+
+  intervalActivateNotifications(){
+  }
+
+
+
+}
+
+const notifications = new Notifications();
+
+export default notifications;
